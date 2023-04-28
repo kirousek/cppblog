e129488e6188ffa2ec19970f53a7d15ac742275b
Patrik Polýnek <87462856+kirousek@users.noreply.github.com>
2023-04-28 10:26:16 +0200

    idk

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,32 +1,67 @@
 const toggleSwitch = document.getElementById("checkbox");
 const currentTheme = sessionStorage.getItem("theme");
 const browserTheme = window.matchMedia("(prefers-color-scheme: dark)");
+
 var clicks = 0;
 
-if (currentTheme) {
-  document.documentElement.setAttribute("color-theme", currentTheme);
-  if (currentTheme === "dark") {
+// Sets color scheme based on default browser settings
+function setDefault() {
+
+  if (currentTheme) {
+    document.documentElement.setAttribute("color-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+      toggleSwitch.checked = true;
+
+    }
+
+  } else if (browserTheme.matches) {
+    document.documentElement.setAttribute("color-theme", "dark");
     toggleSwitch.checked = true;
+
   }
-} else if (browserTheme.matches) {
-  document.documentElement.setAttribute("color-theme", "dark");
-  toggleSwitch.checked = true;
+
 }
 
+// Sets dark color scheme
+function setDark() {
+  document.documentElement.setAttribute("color-theme", "dark");
+  sessionStorage.setItem("theme", "dark");
+
+}
+
+// Sets light color scheme
+function setLight() {
+  document.documentElement.setAttribute("color-theme", "light");
+  sessionStorage.setItem("theme", "light");
+
+}
+
+// Sets secret color scheme
+function setPink() {
+  document.documentElement.setAttribute("color-theme", "pink");
+  sessionStorage.setItem("theme", "pink");
+
+}
+
+// Changes color schemes on click
 function switchTheme(event) {
   clicks += 1;
   console.log(clicks);
+
   if (clicks === 10) {
-    document.documentElement.setAttribute("color-theme", "pink");
-    sessionStorage.setItem("theme", "pink");
+    setPink();
     clicks = 0;
+
   } else if (event.target.checked) {
-    document.documentElement.setAttribute("color-theme", "dark");
-    sessionStorage.setItem("theme", "dark");
+    setDark();
+
   } else {
-    document.documentElement.setAttribute("color-theme", "light");
-    sessionStorage.setItem("theme", "light");
+    setLight();
+
   }
+
 }
 
+setDefault();
 toggleSwitch.addEventListener("change", switchTheme, false);
