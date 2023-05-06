c9e1ef4f44a28d65522526f47ee062368bf35d33
Jakub Rais <65291055+JacobGMS@users.noreply.github.com>
2023-05-06 12:38:16 +0200

    php + remake dotaníku

---
+++ php/odeslat.php
@@ -0,0 +1,19 @@
+<?php
+// Načtěte data z formuláře
+$jmeno = $_POST['jmeno'];
+$email = $_POST['email'];
+$zprava = $_POST['zprava'];
+
+// Nastavte příjemce emailu
+$prijemce = 'tobimichalovsky@gmail.com';
+
+// Nastavte předmět emailu
+$predmet = 'Dotazník z webu';
+
+// Sestavte zprávu
+$zprava_pro_prijemce = "Jméno: $jmeno\nEmail: $email\nZpráva:\n$zprava";
+
+// Odešlete email
+mail($prijemce, $predmet, $zprava_pro_prijemce);
+
+?>
