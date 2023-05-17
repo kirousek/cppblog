45ef19952a90b977a014e1365de77d21ed8ccee9
Patrik Polýnek <87462856+kirousek@users.noreply.github.com>
2023-05-17 11:59:09 +0200

    lol

--- dotaznik/js/send.js
+++ dotaznik/js/send.js
@@ -1,5 +1,14 @@
-const jmeno = document.getElementById("jmeno").value;
-const email = document.getElementById("email").value;
-const zprava = document.getElementById("zprava").value;
-
 window.open("mailto:jacobvideos34@gmail.com?subject=Dotazník%20-%20"+jmeno+"&body="+zprava)
+
+const form  = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const jmeno = document.getElementById("jmeno").value;
+    const email = document.getElementById("email").value;
+    const zprava = document.getElementById("feedback").value;
+
+    window.open("mailto:jacobvideos34@gmail.com?subject=Dotazník%20-%20"+jmeno+"&body="+email+"%0A"+zprava);
+
+});
