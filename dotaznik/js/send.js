58b6c8dba1f32ceca2418eb0e5dedde1a2a764bd
Patrik Polýnek <87462856+kirousek@users.noreply.github.com>
2023-05-17 20:36:34 +0200

    chci se zabit

--- dotaznik/js/send.js
+++ dotaznik/js/send.js
@@ -9,6 +9,6 @@ form.addEventListener('submit', (event) => {
     const email = document.getElementById("email").value;
     const zprava = document.getElementById("feedback").value;
 
-    window.open("mailto:jacobvideos34@gmail.com?subject=Dotazník%20-%20"+jmeno+"&body="+email+"%0A"+zprava);
+    window.open("mailto:jacobvideos34@gmail.com?subject=Dotazník%20-%20"+jmeno+"&body="+email+"%0A"+zprava, "_blank");
 
 });
